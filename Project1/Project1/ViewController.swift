5ce7431f1a47782df6a4113afdee58d75e832c2a
Maksim Nosov <ozolc@yandex.ru>
2019-05-19 10:39:33 +0300

    Project 1: Storm Viewer. Setting up

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Maksim Nosov on 19/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
