7526d0336082f6a738912719bca6d9924402516b
Maksim Nosov <ozolc@yandex.ru>
2019-05-20 10:48:34 +0300

    Final tweaks: hidesBarsOnTap and large titles

--- Project1/Project1/DetailViewController.swift
+++ Project1/Project1/DetailViewController.swift
@@ -16,10 +16,24 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.largeTitleDisplayMode = .never
+        
+        title = selectedImage
+        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
 
 }
