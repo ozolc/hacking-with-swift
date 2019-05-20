6fdac0142f97e93fb6354c5ea83bce1474f803e1
Maksim Nosov <ozolc@yandex.ru>
2019-05-20 12:16:06 +0300

    Project 2: Guess the Flag. Setting up. Designing your layout

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Maksim Nosov on 20/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
