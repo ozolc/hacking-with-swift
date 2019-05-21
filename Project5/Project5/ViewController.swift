a3eafdd3ec881a62ceb5ce4f6e639a822b4ac314
Maksim Nosov <ozolc@yandex.ru>
2019-05-21 17:39:42 +0300

    Project 5: Word Scramble. Setting up

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Maksim Nosov on 21/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
