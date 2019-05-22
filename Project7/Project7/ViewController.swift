641fffeec7338a3f30c87248d33e89a97d70eedf
Maksim Nosov <ozolc@yandex.ru>
2019-05-22 16:06:28 +0300

    Finishing touches: didFinishLaunchingWithOptions

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -9,20 +9,30 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    
     var petitions = [Petition]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-          let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
-//        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            //            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+            //            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        }
         
         if let url = URL(string: urlString) {
             if let data  = try? Data(contentsOf: url) {
                 parse(json: data)
+                return
             }
         }
+        
+        showError()
     }
     
     func parse(json: Data) {
@@ -33,11 +43,17 @@ class ViewController: UITableViewController {
             tableView.reloadData()
         }
     }
-
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
     }
- 
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let petition = petitions[indexPath.row]
@@ -51,5 +67,6 @@ class ViewController: UITableViewController {
         vc.detailItem = petitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
+    
 }
 
