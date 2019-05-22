74958e6e218a804566207cc4af14e19883dcee93
Maksim Nosov <ozolc@yandex.ru>
2019-05-22 14:59:18 +0300

    Project 7: Whitehouse Petitions. Setting up

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Maksim Nosov on 22/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
