17a02a8a48ab3cd3ca50803b16a708a4e1098ee0
Maksim Nosov <ozolc@yandex.ru>
2019-05-22 16:49:02 +0300

    Wrap up

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -11,12 +11,23 @@ import UIKit
 class ViewController: UITableViewController {
     
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
+    var urlString = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString: String
+        let loadDatabutton = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(loadData))
+        let showInfoButton = UIBarButtonItem(title: "Info", style: .done, target: self, action: #selector(showInfo))
+        navigationItem.rightBarButtonItems = [loadDatabutton, showInfoButton]
         
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .done, target: self, action: #selector(filterJSON))
+        
+        loadData()
+    }
+        
+    
+    @objc func loadData() {
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
             //            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
@@ -28,6 +39,7 @@ class ViewController: UITableViewController {
         if let url = URL(string: urlString) {
             if let data  = try? Data(contentsOf: url) {
                 parse(json: data)
+                
                 return
             }
         }
@@ -40,6 +52,7 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            filteredPetitions = petitions
             tableView.reloadData()
         }
     }
@@ -50,13 +63,44 @@ class ViewController: UITableViewController {
         present(ac, animated: true)
     }
     
+    @objc func filterJSON() {
+        let ac = UIAlertController(title: "Enter filter string", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Filter", style: .default) { [weak self, weak ac] _ in
+            guard let filterText = ac?.textFields?[0].text else { return }
+            self?.submit(filterText)
+            self?.tableView.reloadData()
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ text: String) {
+        filteredPetitions.removeAll()
+        for petition in petitions {
+            if petition.title.lowercased().contains(text.lowercased()) {
+                filteredPetitions.append(petition)
+            }
+        }
+//        tableView.reloadData()
+    }
+    
+    
+    @objc func showInfo() {
+        let ac = UIAlertController(title: "Attention", message: "Data comes from the \nWe The People API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -64,7 +108,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
     
