6f1de738a247e469b78177dc3799e0a244691d83
Maksim Nosov <ozolc@yandex.ru>
2019-05-24 09:23:18 +0300

    Project 10: Names to Faces. Designing UICollectionView cells

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Maksim Nosov on 24/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
