0a3accf75536c6e741390f0843ca26e0d018fa60
Maksim Nosov <ozolc@yandex.ru>
2019-05-25 09:47:46 +0300

    Project 13: Instafilter. Designing the interface

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Maksim Nosov on 25/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeFilter(_ sender: UIButton) {
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: UISlider) {
+    }
+}
+
