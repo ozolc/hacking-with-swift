e0e45e6ac2e783fcaaabf133261aa7cf6b82a89e
Maksim Nosov <ozolc@yandex.ru>
2019-05-26 17:29:41 +0300

    Project 18: Debugging

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Maksim Nosov on 26/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+    }
+
+
+}
+
