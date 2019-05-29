4861307b872d538e5c405195ef1fc6366368e0f6
Maksim Nosov <ozolc@yandex.ru>
2019-05-29 11:22:14 +0300

    Writing to iCloud with CloudKit: CKRecord and CKAsset

---
+++ Project33/Project33/SubmitViewController.swift
@@ -0,0 +1,74 @@
+//
+//  SubmitViewController.swift
+//  Project33
+//
+//  Created by Maksim Nosov on 29/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class SubmitViewController: UIViewController {
+
+    var genre: String!
+    var comments: String!
+    
+    var stackView: UIStackView!
+    var status: UILabel!
+    var spinner: UIActivityIndicatorView!
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor.gray
+        
+        stackView = UIStackView()
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        status = UILabel()
+        status.translatesAutoresizingMaskIntoConstraints = false
+        status.text = "Submitting..."
+        status.textColor = UIColor.white
+        status.font = UIFont.preferredFont(forTextStyle: .title1)
+        status.numberOfLines = 0
+        status.textAlignment = .center
+        
+        spinner = UIActivityIndicatorView(style: .whiteLarge)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        spinner.startAnimating()
+        
+        stackView.addArrangedSubview(status)
+        stackView.addArrangedSubview(spinner)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "You're all set!"
+        navigationItem.hidesBackButton = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        doSubmission()
+    }
+    
+    func doSubmission() {
+        
+    }
+    
+    @objc func doneTapped() {
+        _ = navigationController?.popToRootViewController(animated: true)
+    }
+}
