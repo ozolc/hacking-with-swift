45c1cc8728188ca72040a2ebd0261f0eda1f77ec
Maksim Nosov <ozolc@yandex.ru>
2019-05-29 12:29:42 +0300

    Project 38: GitHub Commits. Setting up

---
+++ Project38/Project38/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  Project38
+//
+//  Created by Maksim Nosov on 29/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var detailLabel: UILabel!
+    
+}
---
+++ Project38/Project38/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project38
+//
+//  Created by Maksim Nosov on 29/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
