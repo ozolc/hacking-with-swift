68f63c94f0c392a5bcf741bea212bd0f2b1261fe
Maksim Nosov <ozolc@yandex.ru>
2019-10-07 14:52:05 +0300

    Initial commit. Coordinators01

---
+++ playing-with-samples/Coordinator01/Coordinator01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coordinator01
+//
+//  Created by Maksim Nosov on 07/10/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
